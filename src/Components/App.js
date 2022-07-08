ce2a12aad6bc05cb043656c65836293a41f66697
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-08 12:24:34 -0600

    Add state to App class component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-
-import '../CSS/App.css';
+import '../CSS/App.css'
 
 class App extends Component {
-	
+	constructor() {
+		super()
+		this.state = {
+			movies: []
+		}
+	}
 }
 
-export default App;
+export default App
