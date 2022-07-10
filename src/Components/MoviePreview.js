84377455147c1a4779c552dc5f41f282cb16d2aa
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-07-09 20:33:30 -0700

    create onClick to toggle from all movies to movie preview

--- src/Components/MoviePreview.js
+++ src/Components/MoviePreview.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../CSS/MoviePreview.css'
 
-// const MoviePreview = () => {
-// 	return (
+const MoviePreview = ({ title, overview, posterImage, votes, releaseDate, average, handleChange}) => {
+	return (
+    <article onClick={() => handleChange(title)}>
+      <h2>{title}</h2>
+      <img className="image" src={posterImage} alt={title}></img>
+      <h3>{title}</h3>
+      <p>Average: {average}</p>
+    </article>
 
-// 	)
-// }
+	)
+}
 
 export default MoviePreview
