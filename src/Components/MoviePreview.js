f2865a3e78a7997228d0ce137cce11bb4cc234d8
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-09 22:58:55 -0700

    add styling to MoviePreview

--- src/Components/MoviePreview.js
+++ src/Components/MoviePreview.js
@@ -3,12 +3,16 @@ import '../CSS/MoviePreview.css'
 
 const MoviePreview = ({ movieData, handleChange }) => {
 	return (
-		<main className="preview-container">
-			<article style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`}}onClick={() => handleChange(movieData.title)}>
-				<h2>{movieData.title}</h2>
-				<img className="image" src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} alt={movieData.title}></img>
-				<p>Average: {movieData.vote_average}</p>
-			</article>
+		<main className="preview-container" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`}}>
+			<div className="preview">
+				<article className="display-left" onClick={() => handleChange(movieData.title)}>
+					<img className="movie-image" src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} alt={movieData.title}></img>
+				</article>
+				<article className="display-right">
+					<h2 className="movie-title">{movieData.title}</h2>
+					<p className="movie-info">Average: {movieData.vote_average}</p>
+				</article>
+			</div>
 		</main>
 
 	)
