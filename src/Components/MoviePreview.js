3fff2a8ceb3668a5274c13b6f6d53a5b467b75f8
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-09 21:55:21 -0700

    refactor MoviePreview component

--- src/Components/MoviePreview.js
+++ src/Components/MoviePreview.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../CSS/MoviePreview.css'
 
-const MoviePreview = ({ title, overview, posterImage, votes, releaseDate, average, handleChange}) => {
+const MoviePreview = ({ movieData, handleChange }) => {
 	return (
-    <article onClick={() => handleChange(title)}>
-      <h2>{title}</h2>
-      <img className="image" src={posterImage} alt={title}></img>
-      <h3>{title}</h3>
-      <p>Average: {average}</p>
-    </article>
+		<main className="preview-container">
+			<article style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`}}onClick={() => handleChange(movieData.title)}>
+				<h2>{movieData.title}</h2>
+				<img className="image" src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} alt={movieData.title}></img>
+				<p>Average: {movieData.vote_average}</p>
+			</article>
+		</main>
 
 	)
 }
