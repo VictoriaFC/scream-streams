99c191b659a93110e71d3240a560a9467b96871c
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-07-09 20:32:37 -0700

    create handleChange and renderMOviePreview

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import '../CSS/App.css'
 import Movies from './Movies'
+import MoviePreview from './MoviePreview'
+import MoviePoster from './MoviePoster'
 
 class App extends Component {
 	constructor() {
@@ -9,10 +11,22 @@ class App extends Component {
 			movies: [],
 			innerWidth: window.innerWidth, 
 			error: '',
-      isLoading: true
+      isLoading: true,
+      selectedMovie: "",
+      displaySelected: false,
 		}
 	}
 
+  handleChange = (id) => {
+    const selectedPoster = this.state.movies.find(movie => {
+      return movie.id === id
+    })
+    if(this.state.displaySelected){
+      this.setState({displaySelected: false})
+    }else{
+      this.setState({selectedMovie: selectedPoster, displaySelected: true})
+    }
+  }
 
   componentDidMount(){
     fetch("https://api.themoviedb.org/3/discover/movie?api_key=44887bea2881cacd3e7aa9c9a1e39222&with_genres=27")
@@ -25,13 +39,30 @@ class App extends Component {
 		})
   }
 
+  renderMoviePreview() {
+    return (
+      <MoviePreview 
+        title={this.state.selectedMovie.title}
+        overview={this.state.selectedMovie.overview}
+        average={this.state.selectedMovie.vote_average}
+        votes={this.state.selectedMovie.vote_count}
+        releaseDate={this.state.selectedMovie.release_date}
+        handleChange={this.handleChange}
+        posterImage={`https://image.tmdb.org/t/p/original${this.state.selectedMovie.poster_path}`}
+      />
+    )
+  }
+
   render(){
     return(
       <main className="App">
         <h1>Dont close your eyes</h1>
 				{this.state.error && <h3>{this.state.error}</h3>}
         {this.state.isLoading && <h2>Loading</h2>}
-        <Movies movies={this.state.movies} />
+        { this.state.displaySelected ? 
+          this.renderMoviePreview() :
+          <Movies movies={this.state.movies} handleChange={this.handleChange}  />
+        }
       </main>
     )
   }
