a8ceae6b56b4705fa8213a877fc25d81861b5a1f
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-14 20:34:47 -0700

    Implement Sign In Page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
 import '../CSS/App.css'
+import loadingGif from '../assets/loading.gif'
+import { Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom'
+
+// styling components
+import Nav from './Nav'
+import Header from './Header'
+import Footer from './Footer'
+
+// page components
+import Consent from './Consent'
 import Movies from './Movies'
 import MoviePreview from './MoviePreview'
-import Nav from './Nav'
 import Login from './Login'
 import Logout from './Logout'
 import Favorites from './Favorites'
 import Signup from './Signup'
-import Header from './Header'
-import Footer from './Footer'
-import loadingGif from '../assets/loading.gif'
-import { Route, Switch } from 'react-router-dom';
-
-// pages
-import Consent from './Consent'
-import { Redirect } from 'react-router-dom'
-
 
 
 class App extends Component {
@@ -26,11 +27,33 @@ class App extends Component {
 			innerWidth: window.innerWidth, 
 			error: '',
       isLoading: true,
-      isOfAge: false
+      isOfAge: false, 
+			token: ""
 		}
 	}
 
+	getStateFromLocalStorage() {
+		let token = localStorage.getItem("token")
+		let email = localStorage.getItem("email")
+		let firstName = localStorage.getItem("name")
+		if(token) {
+			this.setState({
+				token: token,
+				email: email,
+				name: firstName,
+				isOfAge: true
+			})
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if(prevState.token !== this.state.token) {
+			this.getStateFromLocalStorage()
+		}
+	}
+	
   componentDidMount(){
+		this.getStateFromLocalStorage();
     fetch("https://api.themoviedb.org/3/discover/movie?api_key=44887bea2881cacd3e7aa9c9a1e39222&with_genres=27")
     .then(response => response.json())
     .then(data => {
@@ -51,7 +74,7 @@ class App extends Component {
 				<Nav />
 				{this.state.error && <h3>{this.state.error}</h3>}
         {this.state.isLoading && <img className="loading-gif" src={loadingGif}/>}
-        {!this.state.isOfAge && <Redirect to="/Consent" />}
+        {localStorage.getItem("isOfAge") !== "true" && <Redirect to="/Consent" />}
         <Switch>
           <Route exact path="/">
             <Header />
@@ -60,18 +83,18 @@ class App extends Component {
           <Route exact path="/Consent">
             <Consent consent={this.userDidConsent}/>
           </Route>
-          {/* <Route exact path="/Login">
+          <Route exact path="/Login">
             <Login />
-          </Route> */}
-          {/* <Route exact path="/Logout">
+          </Route>
+          <Route exact path="/Logout">
             <Logout />
           </Route>
           <Route exact path="/Signup">
             <Signup />
-          </Route> */}
-          {/* <Route exact path="/Favorites">
+          </Route>
+          <Route exact path="/Favorites">
             <Favorites movies={this.state.movies} />
-          </Route> */}
+          </Route>
           <Route exact path="/MoviePreview/:movie_id">
             <MoviePreview />
           </Route>
