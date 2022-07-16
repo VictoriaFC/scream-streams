f7ff0c8c4c1399b856b0ae0ed29734db8b2e4d57
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-16 15:29:51 -0600

    Add logo and github info to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../CSS/Footer.css'
+import logo from '../assets/logo.png'
 
 const Footer = () => {
 	return (
 		<div className="footer">
-			<h3>I will display scream team info soon.</h3>
+			<img className="footer-logo" src={logo}/>
+			<h3 className="footer-header">Created by:</h3>
+			<p> Trish and Victoria </p><br></br>
+			<p>GitHub Usernames: tfoxcollis and VictoriaFC</p>
 		</div>
 	)
 }
