3cc4c84d6cf1870e54bdb8ed32cf9f77e112e5be
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-16 16:27:43 -0600

    Style login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import '../CSS/Nav.css'
 import { Link } from "react-router-dom"
-
+import '../CSS/Login.css'
+import landingImage from '../assets/sinister.gif'
 
 const Login = ({ userLogin }) => {
 	return (
-    <div>
-      <h2>Login</h2>
-      <form className="signup-form" onSubmit={(event) => this.postUserInfo(event)}>
+    <div className="login-container">
+			<img className="login-landing-img"src={landingImage}/>
+      <h2 className="login-header">LOGIN</h2><br></br>
+			<p>To see your WATCH LIST please login</p><br></br>
+      <form className="login-form" onSubmit={(event) => this.postUserInfo(event)}>
 					<label> Email:
-						<input type="text" name="email" placeholder="email" id="email" required/>
+						<br></br><input type="text" name="email" placeholder="email" id="email" required/>
 					</label>
 					<label> Password:
-						<input type="password" name="password" placeholder="password" id="password" required/>
+						<br></br><input type="password" name="password" placeholder="password" id="password" required/>
 					</label>
-					<input type="submit" value="Login" onClick={(event) => userLogin(event)} />
+					<input className="login-button" type="submit" value="Login" onClick={(event) => userLogin(event)} />
 				</form>
       <Link to="/" type="button">
-        <button className="back-button">Back to Main</button>
+        <button className="back-button-login-page">Back to Main</button>
       </Link>
     </div>
 	)
