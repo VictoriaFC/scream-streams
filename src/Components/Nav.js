91d66788a023e937148429243f642792bc935c76
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-16 14:28:08 -0600

    Update styling for Nav bar buttons

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import '../CSS/Nav.css'
-// import logo from "../assets/logo.png"
 import { Link } from "react-router-dom"
-
+import { NavLink } from "react-router-dom"
 
 const Nav = ({ userLogout }) => {
 	return (
 		<nav className="nav-container">
 			<div className="nav-left">
-				{/* <img className="logo" src={logo}/> */}
+				<NavLink to="/">
+					<button className="button">Home</button>
+				</NavLink>
+				
 				{!!sessionStorage.getItem("token") &&
 					<Link to="/Favorites" className="favorite-button">
-						<button className="button">Favorites</button>
+						<button className="button">WATCH LIST</button>
 					</Link>
 				}
 			</div>
 			<div className="nav-right">
 			{!sessionStorage.getItem("token") &&
         <Link to="/Login">
-				  <button className="login-button">Login</button>
+				  <button className="button">Login</button>
         </Link>
 			}
 			{!!sessionStorage.getItem("token") &&
-				<button className="logout-button" onClick={(event) => userLogout(event)}>Logout</button>
+				<button className="button" onClick={(event) => userLogout(event)}>Logout</button>
 			}
 			{!sessionStorage.getItem("token") &&
         <Link to="/SignUp">
-				  <button className="signup-button">Sign Up</button>
+				  <button className="button">Sign Up</button>
         </Link>
 			}
 			</div>
