78738c6080a3e7d516e885f4616fd37ad9bf95dc
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-07-16 22:39:56 -0700

    style button and add freddy quote

--- src/Components/Consent.js
+++ src/Components/Consent.js
@@ -18,19 +18,19 @@ const Consent = ({ consent }) => {
     <div className="consent-container">
 			<img className="consent-logo" src={landingImage}/>
 			<h2 className="consent-header">Welcome to Scream Streams.</h2><br></br>
-			<h3>Looking for some good scary movies to watch?</h3><br></br>
+			<h3>Looking for some scary movies to watch?</h3><br></br>
 			<h4 className="consent-message">Before we let you enter... we gotta ask. 
 				Are you old enough to be watching scary movies? 
 				By checking the box below you are confirming you are at least 17 years old AND 
-				that we are not liable for your NIGHTMARES.</h4>
+				that Scream Streams is not liable for your NIGHTMARES.</h4>
 			<input className="consent-checkbox" type="checkbox" onChange={() => updateCheckbox()}/>
       {console.log(checked)}
       {checked && 
       <>
         <Link to="/" className="consent-button">
-          <br></br><input className="consent-button" type="submit" name="consent" value="CONTINUE" onClick={consent}/>
-        </Link><br></br><br></br>
-        <p>1...2...Freddy's coming for you...</p>
+          <br></br><input className="consent-button" type="submit" name="consent" value="ENTER 💀" onClick={consent}/>
+        </Link>
+        <p className="freddy-quote">"1...2...Freddy's coming for you..."</p>
       
       </>
       }
